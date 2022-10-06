4543ebb7b4d3751948a7eb07b08d10b0b8ce47aa
anichukwuebuka <anichukwuebuka65@gmail.com>
2022-10-07 00:13:11 +0100

    some changes

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ app.use(cors({
 }))
 app.use("*", proxy)
 
-app.listen(4000,() => console.log("listen on port 4000"))
+const PORT = process.env.PORT || 5000
+app.listen(PORT,() => console.log("listen on port 4000"))
