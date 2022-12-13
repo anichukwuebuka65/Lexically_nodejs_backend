cbbce7e907e565dd06f5809e8c9844edfc0f5f5e
anichukwuebuka <anichukwuebuka65@gmail.com>
2022-12-13 22:12:34 +0000

    change again

--- src/proxy.js
+++ src/proxy.js
@@ -1,22 +1,24 @@
-const express = require("express")
-const router = express.Router()
-const axios = require("axios")
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
 
 router.get("/", (req, res) => {
-    const params = new URLSearchParams({
-        client_id: process.env.CLIENT_ID,
-        ...req.query
-    })
-    const BASE_URL = `https://api.unsplash.com${req.baseUrl}?${params}`
+  const params = new URLSearchParams({
+    client_id: process.env.CLIENT_ID,
+    ...req.query,
+  });
+  const BASE_URL = `https://api.unsplash.com${req.baseUrl}?${params}`;
+  console.log("hit");
 
-    axios.get(BASE_URL)
+  axios
+    .get(BASE_URL)
     .then((response) => {
-        res.header(response.headers)
-        res.status(200).json(response.data)
+      res.header(response.headers);
+      res.status(200).json(response.data);
     })
-    .catch(err => {
-      res.status(500).end(err.message)
-    })
-})
+    .catch((err) => {
+      res.status(500).end(err.message);
+    });
+});
 
-module.exports = router
+module.exports = router;
