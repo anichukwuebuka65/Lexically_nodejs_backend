bfee68e1443682d3923c31d8be5589ec2032a3c1
anichukwuebuka <anichukwuebuka65@gmail.com>
2022-12-13 20:16:11 +0000

    change again

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const proxy = require("../src/proxy");
+const proxy = require("./src/proxy");
 const cors = require("cors");
 const { default: axios } = require("axios");
 require("dotenv").config();
@@ -9,13 +9,13 @@ const frontendUrl = "https://lexically-react-frontend.vercel.app/";
 
 app.options("*", cors());
 
-app.get("/api/redirect", (req, res) => {
+app.get("/redirect", (req, res) => {
   res.redirect(
     `https://unsplash.com/oauth/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${backendUrl}/user&response_type=code&scope=public+read_user+read_collections`
   );
 });
 
-app.get("/api/user", (req, res) => {
+app.get("/user", (req, res) => {
   if (req.query.code) {
     axios
       .post("https://unsplash.com/oauth/token", {
@@ -38,10 +38,8 @@ app.get("/api/user", (req, res) => {
     res.redirect(`${frontendUrl}`);
   }
 });
-app.get("/api/hello", () => res.send("hello"));
-app.use("/api/*", proxy);
+app.get("/hello", () => res.send("hello"));
+app.use("*", proxy);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("listen on port 5000"));
-
-module.exports = app;
